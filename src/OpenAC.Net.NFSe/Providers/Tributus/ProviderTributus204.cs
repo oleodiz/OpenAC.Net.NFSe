c16d6bb4113780c6689164f38a3174d35e794c12
oleodiz <leonardosanso@gmail.com>
2025-01-20 15:42:41 -0300

    inicio tributus

---
+++ src/OpenAC.Net.NFSe/Providers/Tributus/ProviderTributus204.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Linq;
+using System.Xml.Linq;
+using OpenAC.Net.Core.Extensions;
+using OpenAC.Net.DFe.Core;
+using OpenAC.Net.DFe.Core.Serializer;
+using OpenAC.Net.NFSe.Commom;
+using OpenAC.Net.NFSe.Commom.Interface;
+using OpenAC.Net.NFSe.Commom.Model;
+using OpenAC.Net.NFSe.Commom.Types;
+using OpenAC.Net.NFSe.Configuracao;
+using OpenAC.Net.NFSe.Nota;
+
+namespace OpenAC.Net.NFSe.Providers;
+
+internal sealed class ProviderTributus204 : ProviderABRASF204
+{
+    #region Constructors
+
+    public ProviderTributus204(ConfigNFSe config, OpenMunicipioNFSe municipio) : base(config, municipio)
+    {
+        Name = "Tributus";
+    }
+
+    #endregion Constructors
+
+    #region Methods
+
+    protected override string GerarCabecalho()
+        => $"<cabecalho versao=\"201001\" {GetNamespace()}><versaoDados>2.04</versaoDados></cabecalho>";
+
+    protected override IServiceClient GetClient(TipoUrl tipo) => new Tributus204ServiceClient(this, tipo);
+
+    #endregion Methods
+}
